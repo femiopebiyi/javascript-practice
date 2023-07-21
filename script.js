d575081d0a7ff550059ef5dde1d55fda2b5e471f
Opebiyi Oluwafemi <opebiyibiodun10@gmail.com>
2023-07-21 23:37:57 +0100

    for loops excercise

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 
-var string= "hello"
-for (let i = 0; i < string.length; i++) {
-    console.log(string[i])
+for (let i = -10;  i<= 19; i++) {
+    console.log(i)
+    
+}
+for(var i=20; i<=40; i+=2){
+    console.log(i)
+
+}
+
+for (let  i = 301; i<= 333;   i+=2) {
+    console.log(i)
     
 }
