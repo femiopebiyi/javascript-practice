d0e103172390d2ee2c11c969ecea4dcbf845d9ca
Opebiyi Oluwafemi <opebiyibiodun10@gmail.com>
2023-07-21 01:44:39 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+var age=18;
+var guess=prompt("what is my age?")
+var myAge=Number(guess);
+if (myAge===18){
+    alert("wow you got that right!!!!")
+}
+else if (myAge<18){
+    alert("go higher dickhead")
+}
+else{
+    alert("go lower hermano")
+}
