6525a394973fd29851d7bd70180915f81e1b186f
Opebiyi Oluwafemi <opebiyibiodun10@gmail.com>
2023-07-21 22:30:45 +0100

    while loops

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 var capital= prompt("what is the capital of nigeria?");
 while (capital.indexOf("Abuja")===-1){
     alert("jesus christ read a book or something")
-    var capital= prompt("what is the capital of nigeria?");
+    var capital= prompt("what is the capital of nigeria??");
 }
 
 alert("you know your geography dont you")
