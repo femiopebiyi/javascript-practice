d163b4394ab7c5db1acb76e3bd6853c1d1ee3f61
Opebiyi Oluwafemi <opebiyibiodun10@gmail.com>
2023-07-31 16:03:41 +0100

    selector methods

--- script.js
+++ script.js
@@ -1,28 +1,6 @@
-var movies= [
-    {
-        title: 'Oppenheimer',
-        star: 5,
-        hasSeen:true
-    },
-    {
-        title:"Barbie",
-        star: 5,
-        hasSeen: false
-    },
-    {
-        title: "John Wick",
-        star : 2,
-        hasSeen: false
-    }
-]
-movies.forEach(function(movie){
-    var result= "You Have ";
-    if(movie.hasSeen){
-        result+= "seen "
-    }
-    else{
-        result+="not seen "
-    }
-    result+=  movie.title + " " + movie.star +"-stars"
-    console.log(result)
-})
+var p = document.querySelector("p")
+
+p.classList.add("big")
+p.classList.remove("big")
+p.classList.toggle("big")
+
