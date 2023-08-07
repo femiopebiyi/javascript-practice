057c8bacecf0ab35a91bbdadf36d2807fb29349e
Opebiyi Oluwafemi <opebiyibiodun10@gmail.com>
2023-08-07 18:03:58 +0100

    todo event

--- script.js
+++ script.js
@@ -1,62 +1,21 @@
-var p1 =document.querySelector("#p1")
-var p2 = document.getElementById ("p2")
-let h1 = document.querySelector("h1")
-let reset = document.querySelector("#reset")
-let input = document.querySelector("input")
-let limitDisplay = document.querySelector('p span')
-let p1count = document.querySelector("#p1count")
-let p2count = document.querySelector("#p2count")
-var p1score = 0
-var p2score = 0
-let winningScore = 5
-var gameOver = false
+let first = document.querySelectorAll("li");
 
-p1.addEventListener("click", function(){
-    if(!gameOver){
-        p1score++
-        if(p1score===winningScore){
-            gameOver = true
-            p1count.classList.toggle("green")
-        }
-        p1count.textContent = p1score
-    }
-    
+
+for(let i =0; i<first.length; i++){
+    first[i].addEventListener("mouseover", function(){
+    this.style.color = "green";
 })
-
-
-
-p2.addEventListener("click", function(){
-    if(!gameOver){
-        p2score++
-        if(p2score===winningScore){
-            gameOver = true
-            p2count.classList.toggle("green")
-        }
-        p2count.textContent = p2score
-        
-    }
+first[i].addEventListener("mouseout", function(){
+    this.style.color="black"
 })
-reset.addEventListener('click', function(){
-    p1score = 0;
-    p2score = 0;
-    p1count.textContent = 0;
-    p2count.textContent = 0;
-    p1count.classList.remove("green");
-    p2count.classList.remove("green");
-    gameOver = false;
-})
-function resett(){
-    p1score = 0;
-    p2score = 0;
-    p1count.textContent = 0;
-    p2count.textContent = 0;
-    p1count.classList.remove("green");
-    p2count.classList.remove("green");
-    gameOver = false;
+    first[i].addEventListener("click", function(){
+        this.classList.toggle("done")
+    })
 }
 
-input.addEventListener("change", function(){
-    limitDisplay.textContent = input.value;
-    winningScore= Number(input.value)
-    resett()
+var gi = [1,2,3,4,5,6,6,7,7,8,8,9,9,9,9,9,9,9,9,9];
+
+
+gi.forEach (function(clout, i){
+    console.log( i + " "+clout)
 })
