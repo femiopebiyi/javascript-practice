c45d37a06cafa82407e0f3af9ef35afaab54e3da
Opebiyi Oluwafemi <opebiyibiodun10@gmail.com>
2023-08-11 03:42:13 +0100

    classes and methods

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
-$("div") .css("backgroundColor", "purple");
-$("div"[0]) .css("backgroundColor", "red");
-$(".highlight") .css("width", "200px");
-$("#third") .css("border", "2px solid orange");
+$("h1") .text("New Text");
+$("li").html("mid")
+$("img").css("width","200px")
+$("input").attr("type", "color")
+$("img").last.attr("src")
+$("input"). addClass("done")
+$("input"). removeClass("done")
+$("input"). toggleClass("done")
