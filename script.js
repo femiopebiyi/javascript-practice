5098db37373bf3bf76bc5799e794f318b9193505
Opebiyi Oluwafemi <opebiyibiodun10@gmail.com>
2023-08-11 02:19:59 +0100

    jQuery

--- script.js
+++ script.js
@@ -1,21 +1,4 @@
-let first = document.querySelectorAll("li");
-
-
-for(let i =0; i<first.length; i++){
-    first[i].addEventListener("mouseover", function(){
-    this.style.color = "green";
-})
-first[i].addEventListener("mouseout", function(){
-    this.style.color="black"
-})
-    first[i].addEventListener("click", function(){
-        this.classList.toggle("done")
-    })
-}
-
-var gi = [1,2,3,4,5,6,6,7,7,8,8,9,9,9,9,9,9,9,9,9];
-
-
-gi.forEach (function(clout, i){
-    console.log( i + " "+clout)
-})
+$("div") .css("backgroundColor", "purple");
+$("div"[0]) .css("backgroundColor", "red");
+$(".highlight") .css("width", "200px");
+$("#third") .css("border", "2px solid orange");
