952fc28f2195a9c7765947f9e24a58b272f26de6
Opebiyi Oluwafemi <opebiyibiodun10@gmail.com>
2023-08-18 01:38:53 +0100

    keypress contd

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 let input = document.querySelector("input");
-$("input").keypress(function (event){
-    if(event.which===13){
-        alert("you pressed enter")
-    }
+// $("input").keypress(function (event){
+//     if(event.which===13){
+//         alert("you pressed enter")
+//     }
+// })
+
+$("h1").on("click", function (){
+    $(this). css("color", "red")
 })
+
+$("input").on("keypress", function(){
+    console.log("dattebayo")
+})
+
