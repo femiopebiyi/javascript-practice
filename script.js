a5675a072d6035c6b8be2dee1ea0843f5aa60da8
Opebiyi Oluwafemi <opebiyibiodun10@gmail.com>
2023-08-18 01:22:45 +0100

    keypress event

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
-$("h1") .text("New Text");
-$("li").html("mid")
-$("img").css("width","200px")
-$("input").attr("type", "color")
-$("img").last.attr("src")
-$("input"). addClass("done")
-$("input"). removeClass("done")
-$("input"). toggleClass("done")
+let input = document.querySelector("input");
+$("input").keypress(function (event){
+    if(event.which===13){
+        alert("you pressed enter")
+    }
+})
